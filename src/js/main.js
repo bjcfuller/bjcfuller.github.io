4c799ee5038f63fad8a3c69db3427593d71768bc
Brandon Fuller <blueplanet419@gmail.com>
2017-11-18 17:23:56 -0500

    Set up gulp, basic file structure and development pipeline

---
+++ src/js/main.js
@@ -0,0 +1,41 @@
+/* General scripts */
+
+
+/*
+ * Load template parts
+ * @param url str the relative url to load
+ * @param el str the element to fill
+ * @param success func optional success function
+ */
+function loadTemplatePart(url, el, success) {
+    
+    'use strict';
+        
+    var xmlhttp = new XMLHttpRequest(), loc, n, i, path, s;
+                
+    xmlhttp.onreadystatechange = function() {
+        if (xmlhttp.readyState == XMLHttpRequest.DONE && xmlhttp.status == 200) {
+            document.querySelector(el).innerHTML = xmlhttp.responseText;
+            if (success) {
+                success();
+            }
+        }
+    };
+    
+    loc = window.location.pathname.split('/');
+    loc.shift();
+        
+    n = (url.match(/\..\//g) || []).length;
+    for(i=0; i<n+1; i++) {
+        loc.pop();
+    }
+    
+    loc = loc.toString();
+    loc = loc.replace(/,/g,'/');
+            
+    path = window.location.protocol + '//' + window.location.hostname + '/' + loc + '/' + url.replace(/\..\//g, '');
+    
+    xmlhttp.open('GET', path, true);
+    xmlhttp.send();
+
+}
